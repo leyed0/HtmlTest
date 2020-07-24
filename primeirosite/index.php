e0dfc2ab94d9808cd13ad4826349c66705ba48aa
leyed0 <orley1994@hotmail.com>
2020-07-24 01:53:48 -0300

    first commit

---
+++ primeirosite/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <title>CI Assistencia tecnica</title>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="Assistencia tecnica - celulares, computadores e eletronicos em geral"/> <!--150 caracteres limite do google-->
+        <meta name="keywords" content="assistencia,celular,computador, notebook"/>
+        <meta name="author" content="Leyed - Orley Eduardo"/>
+        <link rel="stylesheet" href="mainstyle.css"/>
+    </head>
+
+    <body>
+        <div class="lateralmenu">
+            <div class="logo"><a href="#"><img src="img\thumb-grande.jpg" alt=""></a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#" class="">Home</a></li>
+                    <li><a href="#" class="">Cliente</a></li>
+                    <li><a href="#" class="">Serviços</a></li>
+                    <li><a href="#" class="">Produtos</a></li>
+                    <li><a href="#" class="">Sobre</a></li>
+                    <li><a href="#" class="">Ajuda</a></li>
+                </ul>
+            </div>
+        </div>
+        <?php
+        echo "teste";
+        ?>
+    </body>
+</html>
